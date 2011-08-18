717a8a29bf132f0ac0a04c286028d1b11846b5b7
梁宇明 <lyuming@cpan.org>
2011-08-18 05:05:43 +0000

    Import of LYUMING/LYUMING-Dummy-Dist3-0.01 from CPAN.
    gitpan-cpan-distribution: LYUMING-Dummy-Dist3
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LYUMING/LYUMING-Dummy-Dist3-0.01.tar.gz
    gitpan-cpan-author:       LYUMING
    gitpan-cpan-maturity:     released

---
+++ lib/LYUMING/Dummy/Dist3.pm
@@ -0,0 +1,34 @@
+package LYUMING::Dummy::Dist3;
+BEGIN {
+  $LYUMING::Dummy::Dist3::VERSION = '0.01';
+}
+
+# VERSION
+
+1;
+# ABSTRACT: Dummy dist to test PAUSE indexing
+
+__END__
+=pod
+
+=head1 NAME
+
+LYUMING::Dummy::Dist3 - Dummy dist to test PAUSE indexing
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+梁宇明 <yuming888@yahoo.co.id>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by 梁宇明.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/LYUMING/Dummy/Module1.pm
@@ -0,0 +1,35 @@
+package SHARYANTO::Dummy::Module1;
+BEGIN {
+  $SHARYANTO::Dummy::Module1::VERSION = '0.01';
+}
+
+sub from_dist3 {
+}
+
+1;
+# ABSTRACT: Dummy dist to test PAUSE indexing, add functions to Module1
+
+__END__
+=pod
+
+=head1 NAME
+
+SHARYANTO::Dummy::Module1 - Dummy dist to test PAUSE indexing, add functions to Module1
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+梁宇明 <yuming888@yahoo.co.id>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by 梁宇明.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
